5abaf6d720e3ec08a7034bc5d776d43f73fa7ba2
NourSofanati <ultradraftns@gmail.com>
2021-05-18 15:50:30 +0300

    Fix icons in the reports index page

--- resources/views/reports/reportCard.blade.php
+++ resources/views/reports/reportCard.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="block border-8 border-gray-100 h-52 w-36 shadow hover:shadow-md transition-shadow duration-150 ">
         <div class="w-full h-full bg-white grid place-items-center border border-gray-200 hover:border-gray-300">
-            <img src="{{ asset('images/reports/' . $item . '.svg') }}" alt="" class="h-12 w-12">
+            <img src="{{ asset('icons/reports/' . $item . '.svg') }}" alt="" class="h-12 w-12">
             <hr class="border-blue-500 border-2 block w-8/12 ">
             <div
                 class="block bg-gray-100 w-8/12 h-1 rounded hover:animate-pulse hover:bg-gray-200 transition-all duration-75">
