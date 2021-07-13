f9d3224c8025cfd26f71892c0e1f9b3fccd3bc6c
NourSofanati <ultradraftns@gmail.com>
2021-07-13 14:45:40 +0300

    Update

--- resources/views/purchases/create.blade.php
+++ resources/views/purchases/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white shadow-lg rounded">
             <form action="{{ route('fixedAssets.store') }}" method="post">
-                @csrf
+            @csrf
                 <div class="p-5">
                     <div class="mt-3">
                         <label for="name">
